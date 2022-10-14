bc27e8725a99931c9c0d121462f95b8e51892a6c
Isak Svarvar <isak.svarvar@gmail.com>
2022-10-14 19:37:28 +0300

    semi working login

--- renderer.js
+++ renderer.js
@@ -7,11 +7,11 @@
  */
 
 (async () => {
-    console.log(await window.exposed.getStuffFromMain())
+    //console.log(await window.exposed.getStuffFromMain())
 
     await window.exposed.sendStuffToMain('Stuff from renderer')
 })()
 
 document.querySelector('#btn-login').addEventListener('click', async () =>{
-    await window.electron.login()
+    await window.exposed.login()
 })
